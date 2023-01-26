df351d5a3864533f39451eaafbe2b5766067f86b
MauroMoyano <Figurascoleccionablesestudio3D@gmail.com>
2023-01-26 11:21:45 -0300

    primeros test andando

--- FT-M4-master/03-sequelize/homework/db/index.js
+++ FT-M4-master/03-sequelize/homework/db/index.js
@@ -2,12 +2,12 @@ const { Sequelize, Op } = require('sequelize');
 const modelCharacter = require('./models/Character.js');
 const modelAbility = require('./models/Ability.js');
 const modelRole = require('./models/Role.js');
+require("dotenv").config();
 
-const DB_USER = "postgres"
-const DB_PASSWORD = "A77aque77"
-const DB_HOST = "localhost:5432"
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env
 
-const db = new Sequelize('postgres://DB_USER:DB_PASSWORD@DB_HOST/henry_sequelize', {
+const db = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/henry_sequelize`, {
+// const db = new Sequelize(`postgres://postgres:A77aque77@localhost:5432/henry_sequelize`, {
   logging: false,
 });
 
